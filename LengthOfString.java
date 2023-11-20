b40a9f7151b92981fdd601518311012d0c774049
Banu Danabakiyam <130582342+BanuDanabakiyam@users.noreply.github.com>
2023-11-20 05:54:15 +0530

    Add files via upload

---
+++ LengthOfString.java
@@ -0,0 +1,22 @@
+//Write a java program to get the length of a given string.
+import java.util.Scanner;
+class LengthOfString{
+public static int findLength(String dat){
+// '\0' ASCII VALUE 0 ITS DENOTE STRING NULL 
+String d1 = dat + '\0';
+int count = 0;
+// USING FOR LOOP TO CHECK CHARACTER CONTAINS NULL
+for(int i = 0 ; d1.charAt(i) != '\0' ; i++){
+count++;
+}
+return count;
+}
+public static void main(String args[]){
+Scanner scanner = new Scanner(System.in);
+// GET VALUE FORM USER
+System.out.println("Enter Word ");
+String data = scanner.nextLine();
+// DISPLAY RESULT
+System.out.println(findLength(data));
+}
+}
