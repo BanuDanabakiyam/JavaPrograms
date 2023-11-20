1ed3baf178a03bd77c0410756226b3e2350b5071
Banu Danabakiyam <130582342+BanuDanabakiyam@users.noreply.github.com>
2023-11-20 18:06:32 +0530

    Add files via upload

---
+++ AscendingOrder.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class AscendingOrder{
+static Scanner scanner = new Scanner(System.in);
+public static int [] sorted(){
+int arr [] = new int [5];
+System.out.println("Enter 5 values ");
+for(int i = 0 ; i < 5 ; i++){
+int values = scanner.nextInt();
+arr [i] = values;
+}
+System.out.println("Before Ascending Order Sorted : " + Arrays.toString(arr));
+for(int i = 0 ; i < arr.length ; i++){
+for(int j = i + 1 ; j < arr.length ; j++){
+if(arr[i] > arr[j]){
+int temp = arr[j];
+arr[j] = arr[i];
+arr[i] = temp;
+}
+}
+}
+return arr;
+}
+public static void main(String args[]){
+int arr1 [] = sorted();
+System.out.println("After Ascending Order Sorted :" + Arrays.toString(arr1));
+
+}
+}
